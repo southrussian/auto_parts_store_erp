233c3fe87b3d92773cd20a0e6c8064820563c1c6
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2025-05-17 13:01:53 +0300

    Fixed htmls, removed func notify (it's only for mac)

--- carparts/users.py
+++ carparts/users.py
@@ -50,13 +50,3 @@ def setup_user_routes(app):
     def view_users():
         users = User.query.all()
         return render_template('view_users.html', users=users)
-
-
-def notify(title, text, sound):
-    command = '''
-    on run argv 
-        display notification (item 2 of argv) with title (item 1 of argv) sound name (item 3 of argv)
-    end run
-    '''
-
-    subprocess.call(['osascript', '-e', command, title, text, sound])
